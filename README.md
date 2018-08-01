e0e2f7eb8608cc87d57a94665455b93c645db8f0
Reza Aghaei <rag@sitecore.com>
2018-08-01 21:28:57 +0800

    Exmaple created.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Use Blocky in a Desktop Application
+
+This example shows:
+
+ - How you can load a toolbox dynamically
+ - How you can load workspace dynamically
+ - How you can call Blocky API methods using javascript methods. In the example you can see `showCode` and `runCode` proxy methods which are independent from wprkspace and will work with any workspace.  You can call javascript methods from C#.
+
+![Use Blocky in a Desktop Application](blocky.png)
+
+You can use either of [Blocky Demos](https://blockly-demo.appspot.com/static/demos/index.html) for example. I created an example which shows using Blocky API methods and also using Blocky UI Tools. This example is based on [Generating Javascript](https://blockly-demo.appspot.com/static/demos/generator/index.html) example which shows how to use Blocky API to generate a javascript from Blocky workspace.
---
+++ src/BlockyExample/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlockyExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowCodeButton.IsEnabled = false;
+            RunCodeButton.IsEnabled = false;
+            Browser.NavigateToString(System.IO.File.ReadAllText(GetFilePath("blockyHTML.html")));
+        }
+
+        private void RunCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.InvokeScript("runCode", new object[] { });
+        }
+
+        private void ShowCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = Browser.InvokeScript("showCode", new object[] { });
+            MessageBox.Show(result.ToString());
+        }
+
+        private void WebBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            ShowCodeButton.IsEnabled = true;
+            RunCodeButton.IsEnabled = true;
+            var toolboxXML = System.IO.File.ReadAllText(GetFilePath("blockyToolbox.xml"));
+            var workspaceXML = System.IO.File.ReadAllText(GetFilePath("blockyWorkspace.xml"));
+            //Initialize blocky using toolbox and workspace
+            Browser.InvokeScript("init", new object[] { toolboxXML, workspaceXML });
+        }
+
+        private string GetFilePath(string file)
+        {
+            var directory = System.AppDomain.CurrentDomain.BaseDirectory;
+            return System.IO.Path.Combine(directory, file);
+        }
+    }
+}
